cf1e1cefb27ad8dc93ea7f21a872182fd09a6415
Vibha Kawale <vibhakawale06@gmail.com>
2026-05-11 20:59:33 +0530

    Update retail_analysis.sql

--- retail_analysis.sql
+++ retail_analysis.sql
@@ -1,10 +1,12 @@
 USE retail_project;
+GO
 
 -- Total Revenue
 
 SELECT 
     ROUND(SUM(Quantity * UnitPrice), 2) AS total_revenue
 FROM online_retail;
+GO
 
 
 -- Total Orders
@@ -12,28 +14,29 @@ FROM online_retail;
 SELECT 
     COUNT(DISTINCT InvoiceNo) AS total_orders
 FROM online_retail;
+GO
 
 
 -- Top 10 Best Selling Products
 
-SELECT 
+SELECT TOP 10
     Description,
     SUM(Quantity) AS total_quantity
 FROM online_retail
 GROUP BY Description
-ORDER BY total_quantity DESC
-LIMIT 10;
+ORDER BY total_quantity DESC;
+GO
 
 
 -- Top 10 Customers by Spending
 
-SELECT 
+SELECT TOP 10
     CustomerID,
     ROUND(SUM(Quantity * UnitPrice), 2) AS total_spending
 FROM online_retail
 GROUP BY CustomerID
-ORDER BY total_spending DESC
-LIMIT 10;
+ORDER BY total_spending DESC;
+GO
 
 
 -- Monthly Revenue
@@ -44,6 +47,7 @@ SELECT
 FROM online_retail
 GROUP BY MONTH(InvoiceDate)
 ORDER BY month_number;
+GO
 
 
 -- Revenue by Country
@@ -54,24 +58,26 @@ SELECT
 FROM online_retail
 GROUP BY Country
 ORDER BY revenue DESC;
+GO
 
 
 -- Average Order Value
 
 SELECT 
     ROUND(
-        SUM(Quantity * UnitPrice) /
+        SUM(Quantity * UnitPrice) * 1.0 /
         COUNT(DISTINCT InvoiceNo), 2
     ) AS average_order_value
 FROM online_retail;
+GO
 
 
 -- Most Expensive Products
 
-SELECT 
+SELECT TOP 10
     Description,
     MAX(UnitPrice) AS highest_price
 FROM online_retail
 GROUP BY Description
-ORDER BY highest_price DESC
-LIMIT 10;
+ORDER BY highest_price DESC;
+GO
